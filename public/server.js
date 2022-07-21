b0e6ae7f68265c3b88a768bb10970be06abf8307
Nerfez <87040538+Nerfez@users.noreply.github.com>
2022-07-21 16:14:18 +0200

    Add files via upload

---
+++ public/server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname+'/public'));
+
+var vote = {
+	Zefren: 0,
+	Kirai: 0,
+	Extarns: 0
+}
+
+//sockets
+
+io.sockets.on('connection',function(socket){
+	socket.on('user image',function(image){
+		io.sockets.emit('addimage','Nouvel album : ',image);
+	});
+	socket.on('vote', function(data){
+		console.log(data);
+		vote[data]++;
+		io.sockets.emit('updateVote', vote);
+	});
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
+
+http.listen(3000,function(){
+	console.log("Listening on port 3000");
+});
