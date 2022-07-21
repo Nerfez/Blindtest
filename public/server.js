3ff6814f5cc7c959c9a5302df57c4426911b0c0a
Nerfez <87040538+Nerfez@users.noreply.github.com>
2022-07-21 16:15:14 +0200

    Update server.js

--- public/server.js
+++ public/server.js
@@ -28,6 +28,12 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
+/*
 http.listen(3000,function(){
 	console.log("Listening on port 3000");
+});
+*/
+
+app.listen(process.env.PORT, process.env.IP, function () {
+  console.log('Example app started!')
 });
