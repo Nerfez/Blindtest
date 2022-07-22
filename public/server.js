8a158be7c3b042f6df00ae6b7a0455bbd5347da4
Nerfez <87040538+Nerfez@users.noreply.github.com>
2022-07-22 02:04:18 +0200

    Add files via upload

--- public/server.js
+++ public/server.js
@@ -15,19 +15,57 @@ var vote = {
 //sockets
 
 io.sockets.on('connection',function(socket){
+	
+	// Every socket connection has a unique ID
+    console.log('new connection: ' + socket.id);
+	
+	//Récupération de l'image
 	socket.on('user image',function(image){
 		io.sockets.emit('addimage','Nouvel album : ',image);
 	});
+	
+	//Alimente les votes
 	socket.on('vote', function(data){
-		console.log(data);
 		vote[data]++;
 		io.sockets.emit('updateVote', vote);
 	});
+	
+	//Décrémente les votes
+	socket.on('voteDown', function(data){
+		vote[data]--;
+		io.sockets.emit('updateVote', vote);
+	});
+	
+	//pseudonyme s'est connecté
+	socket.on('user name', function(data){
+		io.sockets.emit('updateNewPlayer', data);
+	});
+	
+	//add message
+	socket.on('user message', function(NamePlayer,MyMessage){
+		io.sockets.emit('updateNewMessage', NamePlayer, MyMessage);
+	});
+	
+	//del message
+	socket.on('delete message', function(data){
+		io.sockets.emit('delete chat', data);
+	});
+	
+	//réponse trouvée Chanteur
+	socket.on('correct singer', function(NamePlayer, MyMessage){
+		io.sockets.emit('correct Singer', NamePlayer, MyMessage);
+	});
+	
+	//réponse trouvée Album
+	socket.on('correct album', function(NamePlayer, MyMessage){
+		io.sockets.emit('correct Album', NamePlayer, MyMessage);
+	});
+	
 });
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
-})
+});
 
 
 http.listen(3000,function(){
